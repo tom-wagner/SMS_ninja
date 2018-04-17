25b218fd4f43299f220421a34cff26ef026d3b97
Tom Wagner <wagne868@umn.edu>
2018-04-17 15:56:08 -0400

    Bare-bones, server-side Twilio implementation

--- server/index.js
+++ server/index.js
@@ -2,11 +2,22 @@ var express = require('express');
 var bodyParser = require('body-parser');
 var axios = require('axios');
 var DB = require('./database.js');
+var twilio = require('./twilio.js');
+var {acctSID, authToken, testSID, testToken, twilioNumber} = require('../config.js');
 
 var app = express();
 
 app.use(express.static(__dirname + '/../client/dist'));
 app.use(bodyParser.json());
+
+app.post('/sendSMS', function(req, res) {
+  console.log(req.query.msg);
+  console.log(twilioNumber);
+  twilio.sendSMS(req.query.msg, '+16123603573', twilioNumber).then(result => {
+    console.log(result);
+    res.status(200).send('I think it worked!');
+  })
+});
 
 app.listen(3000, function() {
   console.log('listening on port 3000!');
---
+++ server/twilio.js
@@ -0,0 +1,17 @@
+const {acctSID, authToken, testSID, testToken, twilioNumber} = require('../config.js');
+
+const twilio = require('twilio');
+
+var client = new twilio(acctSID, authToken);
+
+function sendSMS(msg, recipient, sender) {
+  console.log(msg, recipient, sender);
+  return client.messages.create({
+    body: msg,
+    to: recipient,
+    from: sender,
+  });
+}
+
+exports.sendSMS = sendSMS;
+exports.client = client;
