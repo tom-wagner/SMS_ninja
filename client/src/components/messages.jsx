d1f7e947d3ff32d1f009651b2d8c9152f2886315
Tom Wagner <wagne868@umn.edu>
2018-04-22 20:34:25 -0400

    Style scheduledMessages component

--- client/src/components/messages.jsx
+++ client/src/components/messages.jsx
@@ -1,42 +1,35 @@
 import React, { Component } from 'react';
-import {/* REQUIRED COMPONENTS HERE*/} from 'react-bootstrap';
+import { Row, Col } from 'react-bootstrap';
 import { connect } from 'react-redux';
 import { /*TBD*/ } from '../actions/index.js';
 
 class Messages extends Component {
   render() {
-    console.log(this.props.scheduledMessages);
-    // const scheduledMessages = this.props.scheduledMessages.map(message => {
-    //   console.log(message);
-    //   if (message.messageText !== undefined) {
-        {/* <li key={message._id}>
-          <ul>
-            <li>Recipient: {message.recipient}</li> 
-            <li>Message: {message.messageText}</li>
-            <li>Scheduled time: {message.dateTime}</li> 
-          </ul>
-        </li> */}
-    //   }
-    // });
-
     return (
       <div>
-        <ul>
-          {this.props.scheduledMessages.map(message => {
-            if (message.messageText) {
-              let styledNumber = '(' + message.recipient.slice(0, 3) + ') ' + message.recipient.slice(3, 6) + ' - ' + message.recipient.slice(6);
-              
-              return (
-                <li key={message._id}><b>To: {styledNumber}</b>
-                  <ul>
-                    <li>Message: {message.messageText}</li>
-                    <li>Scheduled time: {message.dateTime}</li>
-                  </ul>
-                </li>
-              );
-            }
-          })}
-        </ul>
+        <Row className="show-grid">
+          <Col xs={8} xsOffset={2}>
+            <div className="messages-container">
+              <h1 className="form-header">Scheduled Messages</h1>
+              <ul>
+                {this.props.scheduledMessages.map(message => {
+                  if (message.messageText) {
+                    let styledNumber = '(' + message.recipient.slice(0, 3) + ') ' + message.recipient.slice(3, 6) + ' - ' + message.recipient.slice(6);
+                    
+                    return (
+                      <li key={message._id}><b>To: {styledNumber}</b>
+                        <ul>
+                          <li>Message: {message.messageText}</li>
+                          <li>Scheduled time: {message.dateTime}</li>
+                        </ul>
+                      </li>
+                    );
+                  }
+                })}
+              </ul>
+            </div>
+          </Col>
+        </Row>
       </div>
     );
   }
@@ -45,11 +38,11 @@ class Messages extends Component {
 const mapStateToProps = (state) => {
   return {
     scheduledMessages: state.scheduledMessages,
-    username: state.username,
-    phoneNumber: state.phoneNumber,
-    isLoggedIn: state.isLoggedIn,
-    previouslySentMessages: state.previouslySentMessages,
-    view: state.view,
+    // username: state.username,
+    // phoneNumber: state.phoneNumber,
+    // isLoggedIn: state.isLoggedIn,
+    // previouslySentMessages: state.previouslySentMessages,
+    // view: state.view,
   }
 }
 
