f4ae6b369913cd430dca88ba303c64a6598e6e3c
Tom Wagner <wagne868@umn.edu>
2018-04-23 11:01:02 -0400

    Require momentJS library and style dates

--- client/src/components/messages.jsx
+++ client/src/components/messages.jsx
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import { Row, Col } from 'react-bootstrap';
 import { connect } from 'react-redux';
 import { /*TBD*/ } from '../actions/index.js';
+import moment from 'moment';
 
 class Messages extends Component {
   render() {
@@ -15,12 +16,12 @@ class Messages extends Component {
                 {this.props.scheduledMessages.reverse().map((message, id) => {
                   if (message.messageText) {
                     let styledNumber = '(' + message.recipient.slice(0, 3) + ') ' + message.recipient.slice(3, 6) + ' - ' + message.recipient.slice(6);
-                    
+                    let date = moment(message.dateTime).format('MMMM Do YYYY, h:mm a');
                     return (
                       <li key={id}><b>To: {styledNumber}</b>
                         <ul>
                           <li>Message: {message.messageText}</li>
-                          <li>Scheduled time: {message.dateTime}</li>
+                          <li><i>Scheduled time: {date}</i></li>
                         </ul>
                       </li>
                     );
